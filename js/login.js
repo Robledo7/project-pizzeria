fb7e5b72f941df3a023bb9af7857b474f891459b
Anilu Robledo <lurobledo.scl@gmail.com>
2024-08-28 20:40:35 -0300

    first commit

--- js/login.js
+++ js/login.js
@@ -1,12 +1,23 @@
+const errorMessages = document.getElementById("errorMessages");
 const usrname = document.getElementById("username");
 const pass = document.getElementById("password");
 const btn = document.getElementById("login");
-//instrucciones cuando aparecen llaves//
-btn.addEventListener("click", (e) => { 
+
+
+function displayError(message) {
+  errorMessages.innerHTML = message;
+}
+btn.addEventListener("click", (e) => {
+  //e.preventDefault(); *// //insrtucciones de la funcion */
+  const data = {
+    username: usrname.value,
+    password: pass.value
+  }
+  console.log(data);
+  const passValue = document.getElementById("password").value;
+  if (passValue.length < 6) {
     e.preventDefault();
-    const data={
-        username:usrname.value,
-        password:pass.value
-       }
-     console.log(data);
+    //alert('El Password debe tener como minimo 6 caracteres de longitud.');
+    displayError("El Password debe tener como minimo 6 caracteres de longitud.");
+  }
 });
